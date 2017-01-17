48ba8202f0efc7a2af49d65d34a9b45a193a49e4
Nguyễn Tuấn Hùng <llg94th@gmail.com>
2017-01-17 15:26:23 +0700

    Tèn ten

--- app/src/main/java/com/xtelsolution/xmec/MainActivity.java
+++ app/src/main/java/com/xtelsolution/xmec/MainActivity.java
@@ -1,13 +1,27 @@
 package com.xtelsolution.xmec;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BaseView{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+    }
+
+    @Override
+    public Activity getActivity() {
+        return this;
+    }
+
+    @Override
+    public void showToast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
