81329bd7b7d157e98ffb33b3704e5142945bf572
Nguyễn Tuấn Hùng <llg94th@gmail.com>
2017-01-18 17:14:02 +0700

    Update Fragment Search 2

---
+++ app/src/main/java/com/xtelsolution/xmec/xmec/views/adapter/IllnessAdapter.java
@@ -0,0 +1,42 @@
+package com.xtelsolution.xmec.xmec.views.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xtelsolution.xmec.R;
+
+/**
+ * Created by HUNGNT on 1/18/2017.
+ */
+
+public class IllnessAdapter extends RecyclerView.Adapter {
+    private Context mContext;
+
+    public IllnessAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.list_item_illness, null);
+        return new IllnessAdapter.IllnessViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    private class IllnessViewHolder extends RecyclerView.ViewHolder {
+        public IllnessViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/com/xtelsolution/xmec/xmec/views/fragment/SearchFragment.java
+++ app/src/main/java/com/xtelsolution/xmec/xmec/views/fragment/SearchFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.xtelsolution.xmec.R;
+import com.xtelsolution.xmec.xmec.views.adapter.IllnessAdapter;
 import com.xtelsolution.xmec.xmec.views.adapter.NewsAdapter;
 import com.xtelsolution.xmec.xmec.views.smallviews.RecyclerViewMargin;
 
@@ -20,6 +21,7 @@ import com.xtelsolution.xmec.xmec.views.smallviews.RecyclerViewMargin;
 public class SearchFragment extends Fragment {
 
     private RecyclerView rvResultFindNews;
+    private RecyclerView rvResultFindIllness;
 
     @Nullable
     @Override
@@ -35,15 +37,21 @@ public class SearchFragment extends Fragment {
     }
 
     private void initRecyclerView() {
+        //////////////////////////////////////////////////
         NewsAdapter adapter = new NewsAdapter(getContext());
         rvResultFindNews.setAdapter(adapter);
         rvResultFindNews.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
         RecyclerViewMargin decoration = new RecyclerViewMargin((int)(getContext().getResources().getDisplayMetrics().density*8f+0.5f));
         rvResultFindNews.addItemDecoration(decoration);
+        ////
+        IllnessAdapter illnessAdapter = new IllnessAdapter(getContext());
+        rvResultFindIllness.setAdapter(illnessAdapter);
+        rvResultFindIllness.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
     }
 
     private void initView() {
         rvResultFindNews = (RecyclerView) getActivity().findViewById(R.id.rvResultFindNews);
+        rvResultFindIllness = (RecyclerView) getActivity().findViewById(R.id.rvResultFindIllnesses);
 
     }
 }
