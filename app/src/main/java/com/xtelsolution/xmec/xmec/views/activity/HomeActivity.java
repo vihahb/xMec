87a7af814e0966723801ea84a9d248e180b85bb5
phimhph04071 <santafe9xyb@gmail.com>
2017-01-19 20:17:15 +0700

    fragment hospital center

--- app/src/main/java/com/xtelsolution/xmec/xmec/views/activity/HomeActivity.java
+++ app/src/main/java/com/xtelsolution/xmec/xmec/views/activity/HomeActivity.java
@@ -1,15 +1,25 @@
 package com.xtelsolution.xmec.xmec.views.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.SlidingDrawer;
 
 import com.xtelsolution.xmec.R;
+import com.xtelsolution.xmec.xmec.views.adapter.HospitalCenterAdapter;
 import com.xtelsolution.xmec.xmec.views.fragment.HomeFragment;
 import com.xtelsolution.xmec.xmec.views.fragment.MapFragment;
 import com.xtelsolution.xmec.xmec.views.fragment.MedicineFragment;
@@ -26,14 +36,20 @@ public class HomeActivity extends AppCompatActivity {
     private SpaceTabLayout tabLayout;
     private ViewPager viewPager;
     private List<Fragment> fragmentList;
-
+    private SlidingDrawer slidingDrawer;
+    private RecyclerView rvHosiptalCenter;
+    private HospitalCenterAdapter adapter;
+    private ImageView imgHanderSliding;
+    private String TAG="phimau";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         init();
         tabLayout.initialize(viewPager, getSupportFragmentManager(), fragmentList, savedInstanceState);
-
+        rvHosiptalCenter.setAdapter(adapter);
+        rvHosiptalCenter.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        slidingDrawer.setVisibility(View.GONE);
     }
 
 
@@ -49,6 +65,31 @@ public class HomeActivity extends AppCompatActivity {
         fragmentList.add(new HomeFragment());
         fragmentList.add(new NewsFeedFragment());
         fragmentList.add(new MapFragment());
+        slidingDrawer = (SlidingDrawer) findViewById(R.id.drawer);
+        rvHosiptalCenter = (RecyclerView) slidingDrawer.findViewById(R.id.rv_hospital_center);
+        adapter = new HospitalCenterAdapter(getApplicationContext());
+        imgHanderSliding = (ImageView) slidingDrawer.findViewById(R.id.handleImageView);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                if (position==4){
+                    slidingDrawer.setVisibility(View.VISIBLE);
+                } else {
+                    slidingDrawer.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
 
     @Override
--- app/src/main/java/com/xtelsolution/xmec/xmec/views/fragment/MapFragment.java
+++ app/src/main/java/com/xtelsolution/xmec/xmec/views/fragment/MapFragment.java
@@ -3,20 +3,58 @@ package com.xtelsolution.xmec.xmec.views.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SlidingDrawer;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.xtelsolution.xmec.R;
+import com.xtelsolution.xmec.xmec.views.adapter.HospitalCenterAdapter;
 
 /**
  * Created by HUNGNT on 1/18/2017.
  */
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+    private View view;
+    private SlidingDrawer slidingDrawer;
+    private RecyclerView rvHosiptalCenter;
+    private HospitalCenterAdapter adapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main, container, false);
+
+        if (view == null) {
+            view = inflater.inflate(R.layout.fragment_health_center, container, false);
+//            slidingDrawer = (SlidingDrawer) view.findViewById(R.id.drawer);
+//            rvHosiptalCenter = (RecyclerView) slidingDrawer.findViewById(R.id.rv_hospital_center);
+//            adapter = new HospitalCenterAdapter(getContext());
+//            rvHosiptalCenter.setAdapter(adapter);
+//            rvHosiptalCenter.setLayoutManager(new LinearLayoutManager(getContext()));
+        }
+        return view;
+
+    }
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initMap();
+    }
+
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
