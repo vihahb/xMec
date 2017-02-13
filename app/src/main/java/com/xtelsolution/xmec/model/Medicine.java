4a0706d4ab7984bef051e03771b16bd5377e2df6
phimhph04071 <santafe9xyb@gmail.com>
2017-02-13 17:30:25 +0700

    Medicine data

--- app/src/main/java/com/xtelsolution/xmec/model/Medicine.java
+++ app/src/main/java/com/xtelsolution/xmec/model/Medicine.java
@@ -8,14 +8,61 @@ public class Medicine {
     private int id;
     private String name;
     private String type;
+    private String group;
+    private String component;
+    private String indication;
+    private String contraindication;
+    private String drugInteraction;
+    private String sidEeffect;
+    private String warning;
+    private String dosage;
+    private String preservation;
 
-    @Override
-    public String toString() {
-        return "Medicine{" +
-                "type='" + type + '\'' +
-                ", name='" + name + '\'' +
-                ", id=" + id +
-                '}';
+    public Medicine(int id, String name, String type, String group, String component, String indication, String contraindication, String drugInteraction, String sidEeffect, String warning,String dosage, String preservation) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.group = group;
+        this.component = component;
+        this.indication = indication;
+        this.contraindication = contraindication;
+        this.drugInteraction = drugInteraction;
+        this.sidEeffect = sidEeffect;
+        this.warning = warning;
+        this.dosage =dosage;
+        this.preservation = preservation;
+    }
+
+    public String getComponent() {
+        return component;
+    }
+
+    public void setComponent(String component) {
+        this.component = component;
+    }
+
+    public String getContraindication() {
+        return contraindication;
+    }
+
+    public void setContraindication(String contraindication) {
+        this.contraindication = contraindication;
+    }
+
+    public String getDrugInteraction() {
+        return drugInteraction;
+    }
+
+    public void setDrugInteraction(String drugInteraction) {
+        this.drugInteraction = drugInteraction;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
     }
 
     public int getId() {
@@ -26,12 +73,36 @@ public class Medicine {
         this.id = id;
     }
 
+    public String getIndication() {
+        return indication;
+    }
+
+    public void setIndication(String indication) {
+        this.indication = indication;
+    }
+
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPreservation() {
+        return preservation;
+    }
+
+    public void setPreservation(String preservation) {
+        this.preservation = preservation;
+    }
+
+    public String getSidEeffect() {
+        return sidEeffect;
+    }
+
+    public void setSidEeffect(String sidEeffect) {
+        this.sidEeffect = sidEeffect;
     }
 
     public String getType() {
@@ -42,14 +113,37 @@ public class Medicine {
         this.type = type;
     }
 
-    public Medicine() {
-
+    public String getWarning() {
+        return warning;
     }
 
-    public Medicine(int id, String name, String type) {
+    public void setWarning(String warning) {
+        this.warning = warning;
+    }
 
-        this.id = id;
-        this.name = name;
-        this.type = type;
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+
+    @Override
+    public String toString() {
+        return "Medicine{" +
+                "component='" + component + '\'' +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", group='" + group + '\'' +
+                ", indication='" + indication + '\'' +
+                ", contraindication='" + contraindication + '\'' +
+                ", drugInteraction='" + drugInteraction + '\'' +
+                ", sidEeffect='" + sidEeffect + '\'' +
+                ", warning='" + warning + '\'' +
+                ", dosage='" + dosage + '\'' +
+                ", preservation='" + preservation + '\'' +
+                '}';
     }
 }
