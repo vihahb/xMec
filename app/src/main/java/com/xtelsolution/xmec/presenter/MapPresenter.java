bf93a357550b9adc261916b7f3c116e92893d6d9
phimhph04071 <santafe9xyb@gmail.com>
2017-03-27 16:36:06 +0700

    fix location map

--- app/src/main/java/com/xtelsolution/xmec/presenter/MapPresenter.java
+++ app/src/main/java/com/xtelsolution/xmec/presenter/MapPresenter.java
@@ -10,9 +10,15 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
@@ -37,47 +43,46 @@ import java.util.List;
  * Created by phimau on 3/1/2017.
  */
 
-public class MapPresenter extends BasePresenter {
+public class MapPresenter extends BasePresenter implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
     private IMapView view;
     private Activity mActivity;
     private LocationManager mLocationManager;
     private double lat;
     private double log;
     private double radius;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
     private HashMap<Integer, Boolean> listPos;
     private final int GETLOCATION = 1;
     private boolean isMapInit = false;
-    private final LocationListener mLocationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            lat = location.getLatitude();
-            log = location.getLongitude();
-            if (isMapInit)
-                view.onLocationChange(new LatLng(lat, log));
-        }
 
-        @Override
-        public void onStatusChanged(String s, int i, Bundle bundle) {
-            Log.d("MapPresenter", "onStatusChanged: ");
-        }
-
-        @Override
-        public void onProviderEnabled(String s) {
-            Log.d("MapPresenter", "onProviderEnabled: ");
-        }
-
-        @Override
-        public void onProviderDisabled(String s) {
-            Log.d("MapPresenter", "onProviderDisabled: ");
-        }
-    };
+//    private final LocationListener mLocationListener = new LocationListener() {
+//        @Override
+//        public void onLocationChanged(Location location) {
+//
+//        }
+//
+//        @Override
+//        public void onStatusChanged(String s, int i, Bundle bundle) {
+//            Log.d("MapPresenter", "onStatusChanged: ");
+//        }
+//
+//        @Override
+//        public void onProviderEnabled(String s) {
+//            Log.d("MapPresenter", "onProviderEnabled: ");
+//        }
+//
+//        @Override
+//        public void onProviderDisabled(String s) {
+//            Log.d("MapPresenter", "onProviderDisabled: ");
+//        }
+//    };
 
     public MapPresenter(IMapView view) {
         this.view = view;
         mActivity = view.getActivity();
         radius = 500;
         listPos = new HashMap<>();
-
     }
 
     public void getCurrentLocation() {
@@ -86,36 +91,33 @@ public class MapPresenter extends BasePresenter {
             return;
         }
         view.onGetCurrentLocationFinish(new LatLng(lat, log));
-
     }
 
     public void initMap() {
-        isMapInit =true;
+        isMapInit = true;
         view.onLocationChange(new LatLng(lat, log));
     }
 
     public void initPermission() {
-
         mLocationManager = (LocationManager) view.getActivity().getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(mActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(mActivity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             view.getFragmentView().requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 99);
             return;
         } else {
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 300, 10, mLocationListener);
-            Location lastKnownLocationGPS = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            if (lastKnownLocationGPS != null) {
-                lat = lastKnownLocationGPS.getLatitude();
-                log = lastKnownLocationGPS.getLongitude();
-                Log.e("MY", "initPermission: " + lat + "    " + log);
-            }
             view.onPermissionGranted();
+
+            if (googleApiClient == null) {
+                googleApiClient = new GoogleApiClient.Builder(mActivity)
+                        .addConnectionCallbacks(this)
+                        .addOnConnectionFailedListener(this)
+                        .addApi(LocationServices.API)
+                        .build();
+            }
+            googleApiClient.connect();
         }
-
-
     }
 
     public void getHospitals(final Object... param) {
-
         String location = "lat=" + lat + "&lng=" + log;
         String url = Constant.SERVER_XMEC + Constant.HEALTHY_CENTER + "?" + location + "&radius=" + radius;
         HealthyCareModel.getInstance().getHospital(url, LoginManager.getCurrentSession(), new ResponseHandle<RESP_List_Map_Healthy_Care>(RESP_List_Map_Healthy_Care.class) {
@@ -161,15 +163,69 @@ public class MapPresenter extends BasePresenter {
             }
     }
 
+    private void createLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(100);
+        locationRequest.setFastestInterval(100);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
     @Override
     public void onGetNewSessionSuccess(Object... param) {
         switch ((int) param[0]) {
             case GETLOCATION:
                 getHospitals(param);
                 break;
-//            case GETMEDICAL:
-//                getMedicalReportBooks();
-//                break;
         }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(mActivity, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED && mLocationManager.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER)) {
+            createLocationRequest();
+            startLocationUpdates();
+            Location location = LocationServices.FusedLocationApi.getLastLocation(
+                    googleApiClient);
+            if (location != null) {
+                lat = location.getLatitude();
+                log = location.getLongitude();
+                view.onGetCurrentLocationFinish(new LatLng(lat, log));
+            }
+        }
+
+    }
+
+    private void startLocationUpdates() {
+        if (googleApiClient == null) {
+            xLog.e("client is null");
+        }
+        if (locationRequest == null) {
+            xLog.e("location request is null");
+        }
+        if (ContextCompat.checkSelfPermission(mActivity, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED && mLocationManager.isProviderEnabled(android.location.LocationManager.GPS_PROVIDER)) {
+
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        lat = location.getLatitude();
+        log = location.getLongitude();
+        if (isMapInit)
+            view.onLocationChange(new LatLng(lat, log));
     }
 }
