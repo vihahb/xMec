6283b8514e8cbf7b8ffbd83bebf41da44145b892
chungdt <chungdt@elcom.com>
2017-05-23 17:20:47 +0700

    viewpage home 90%

---
+++ app/src/main/java/com/xtelsolution/xmec/xmec/views/adapter/PagerUserAdapter.java
@@ -0,0 +1,192 @@
+package com.xtelsolution.xmec.xmec.views.adapter;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.xtelsolution.xmec.R;
+import com.xtelsolution.xmec.model.RESP_User;
+import com.xtelsolution.xmec.model.SharedPreferencesUtils;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import agency.tango.android.avatarview.IImageLoader;
+import agency.tango.android.avatarview.loader.PicassoLoader;
+import agency.tango.android.avatarview.views.AvatarView;
+
+/**
+ * Created by Admin on 5/23/2017.
+ */
+
+public class PagerUserAdapter extends RecyclerView.Adapter {
+    private List<RESP_User> data;
+    private Context mContext;
+    IImageLoader imageLoader;
+    private final int TYPE_ITEM = 0;
+    private ItemUpDateClickListener itemUpDateClickListener;
+
+    public PagerUserAdapter(Context mContext, List<RESP_User> data) {
+        this.mContext = mContext;
+        this.data = data;
+        imageLoader = new PicassoLoader();
+    }
+
+    public void setItemUpDateClickListener(ItemUpDateClickListener itemUpDateClickListener) {
+        this.itemUpDateClickListener = itemUpDateClickListener;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == TYPE_ITEM) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_viewpage_user, parent, false);
+            return new UserViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_viewpage_add_user, parent, false);
+            return new AddUserViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof UserViewHolder) {
+
+            UserViewHolder viewHolder = (UserViewHolder) holder;
+            viewHolder.setData(position);
+        } else {
+            AddUserViewHolder addUserViewHolder = (AddUserViewHolder) holder;
+            addUserViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(mContext, "add thành viên", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size() + 1;
+    }
+
+    private class AddUserViewHolder extends RecyclerView.ViewHolder {
+        LinearLayout add_layout;
+
+        public AddUserViewHolder(View itemView) {
+            super(itemView);
+            add_layout = (LinearLayout) itemView.findViewById(R.id.add_layout);
+        }
+    }
+
+    private class UserViewHolder extends RecyclerView.ViewHolder {
+
+        private AvatarView imgAvatar;
+        private TextView btnProfile;
+        private TextView tvName;
+        private TextView tvBirthday;
+        private TextView tvHeight;
+        private TextView tvWeight;
+        private TextView tvIBM;
+        private ImageView imgGender;
+        private CoordinatorLayout progcess;
+
+        private UserViewHolder(View view) {
+            super(view);
+            btnProfile = (TextView) view.findViewById(R.id.btnProfile);
+            tvBirthday = (TextView) view.findViewById(R.id.tv_birthday);
+            tvHeight = (TextView) view.findViewById(R.id.tv_profile_height);
+            tvWeight = (TextView) view.findViewById(R.id.tv_profile_weight);
+            tvIBM = (TextView) view.findViewById(R.id.tv_profile_ibm);
+            tvName = (TextView) view.findViewById(R.id.tv_profile_name);
+            imgAvatar = (AvatarView) view.findViewById(R.id.img_avatar);
+            imgGender = (ImageView) view.findViewById(R.id.img_gender);
+            progcess = (CoordinatorLayout) view.findViewById(R.id.progress_bar);
+        }
+
+        private void setData(final int position) {
+            final RESP_User user = data.get(position);
+            if (user == null) {
+
+                return;
+            }
+            imageLoader.loadImage(imgAvatar, "http:/example.com/user/someUserAvatar.png", user.getFullname());
+            tvName.setText(user.getFullname());
+            tvBirthday.setText(user.getBirthDayasString());
+            tvHeight.setText(String.valueOf(user.getHeight()));
+            tvWeight.setText(String.valueOf(user.getWeight()));
+            try {
+                DecimalFormat format = new DecimalFormat("##.##");
+                tvIBM.setText(format.format(user.getWeight() * 100 * 100 / (user.getHeight() * user.getHeight())));
+            } catch (Exception e) {
+                tvIBM.setText("0.0");
+            }
+
+
+            if (user.getGender() == 2) {
+//                imgGender.setVisibility(View.VISIBLE);
+                imgGender.setImageResource(R.drawable.ic_action_name);
+            } else if (user.getGender() == 1) {
+//                imgGender.setVisibility(View.VISIBLE);
+                imgGender.setImageResource(R.drawable.ic_man);
+            } else {
+                imgGender.setVisibility(View.GONE);
+            }
+            SharedPreferencesUtils.getInstance().saveUser(user);
+            progcess.setVisibility(View.GONE);
+            btnProfile.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (itemUpDateClickListener != null)
+                        itemUpDateClickListener.onClick(position, user);
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position < data.size() ? TYPE_ITEM : 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public void add(RESP_User user) {
+        insert(user, data.size());
+    }
+
+    public void insert(RESP_User user, int position) {
+        data.add(position, user);
+        notifyItemInserted(position);
+    }
+
+    public void remove(int position) {
+        data.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void clear() {
+        int size = data.size();
+        data.clear();
+        notifyItemRangeRemoved(0, size);
+    }
+
+    public void addAll(List<RESP_User> list) {
+        int startIndex = data.size();
+        data.addAll(startIndex, list);
+        notifyItemRangeInserted(startIndex, list.size());
+    }
+
+    public interface ItemUpDateClickListener {
+        void onClick(int position, RESP_User user);
+    }
+}
--- app/src/main/java/com/xtelsolution/xmec/xmec/views/fragment/HomeFragment.java
+++ app/src/main/java/com/xtelsolution/xmec/xmec/views/fragment/HomeFragment.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.widget.NestedScrollView;
@@ -35,7 +36,9 @@ import com.xtelsolution.xmec.xmec.views.activity.AddMedicalDetailActivity;
 import com.xtelsolution.xmec.xmec.views.activity.MedicalDetailActivity;
 import com.xtelsolution.xmec.xmec.views.activity.ProfileActivity;
 import com.xtelsolution.xmec.xmec.views.adapter.MedicalDirectoryAdapter;
+import com.xtelsolution.xmec.xmec.views.adapter.PagerUserAdapter;
 import com.xtelsolution.xmec.xmec.views.inf.IHomeView;
+import com.yarolegovich.discretescrollview.DiscreteScrollView;
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -64,18 +67,21 @@ public class HomeFragment extends BasicFragment implements ScreenShotable, IHome
     private RESP_User userModel;
     private MedicalDirectoryAdapter adapter;
     private RecyclerView rvDisease;
-    private ImageView imgAvatar;
-    private TextView btnProfile;
-    private TextView tvName;
-    private TextView tvBirthday;
-    private TextView tvHeight;
-    private TextView tvWeight;
-    private TextView tvIBM;
+    private DiscreteScrollView pagerUser;
+    private PagerUserAdapter userAdapter;
+    //    private ImageView imgAvatar;
+//    private TextView btnProfile;
+//    private TextView tvName;
+//    private TextView tvBirthday;
+//    private TextView tvHeight;
+//    private TextView tvWeight;
+//    private TextView tvIBM;
     private HomePresenter presenter;
     private Context mContext;
     private ArrayList<RESP_Medical> mlistMedica;
-    private ImageView imgGender;
-    private CoordinatorLayout progcess;
+    private ArrayList<RESP_User> mlistUsers;
+//    private ImageView imgGender;
+//    private CoordinatorLayout progcess;
 
 
     @Override
@@ -84,9 +90,12 @@ public class HomeFragment extends BasicFragment implements ScreenShotable, IHome
         mContext = getContext();
         presenter = new HomePresenter(this);
         mlistMedica = new ArrayList<>();
+        mlistUsers = new ArrayList<>();
         adapter = new MedicalDirectoryAdapter(mlistMedica, getContext());
         adapter.setItemClickListener(this);
         adapter.setButtonAdapterClickListener(this);
+        userAdapter = new PagerUserAdapter(mContext, mlistUsers);
+
     }
 
     @Nullable
@@ -103,61 +112,83 @@ public class HomeFragment extends BasicFragment implements ScreenShotable, IHome
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initUI(view);
+
+    }
+
+    public void initUI(View view) {
+//        btnProfile = (TextView) view.findViewById(R.id.btnProfile);
+//        tvBirthday = (TextView) view.findViewById(R.id.tv_birthday);
+//        tvHeight = (TextView) view.findViewById(R.id.tv_profile_height);
+//        tvWeight = (TextView) view.findViewById(R.id.tv_profile_weight);
+//        tvIBM = (TextView) view.findViewById(R.id.tv_profile_ibm);
+//        tvName = (TextView) view.findViewById(R.id.tv_profile_name);
+//        imgAvatar = (ImageView) view.findViewById(R.id.img_avatar);
+//        imgGender = (ImageView) view.findViewById(R.id.img_gender);
+//        progcess = (CoordinatorLayout) view.findViewById(R.id.progress_bar);
         rvDisease = (RecyclerView) view.findViewById(R.id.rvDisease);
+        pagerUser = (DiscreteScrollView) view.findViewById(R.id.pagerUser);
         this.view = view;
         containerView = view.findViewById(R.id.container);
         LinearLayoutManager manager = new LinearLayoutManager(getContext());
         rvDisease.setLayoutManager(manager);
         rvDisease.setNestedScrollingEnabled(false);
         rvDisease.setAdapter(adapter);
-        NestedScrollView scrollView = (NestedScrollView) view.findViewById(R.id.scrollView);
-        scrollView.setOnScrollChangeListener(new EndlessParentScrollListener(manager) {
-            @Override
-            public void onLoadMore(int page, int totalItemsCount) {
 
-            }
 
-            @Override
-            public void onHide() {
-                mContext.sendBroadcast(new Intent(Constant.ACTION_HIDE_BOTTOM_BAR));
-            }
-
-            @Override
-            public void onShow() {
-                mContext.sendBroadcast(new Intent(Constant.ACTION_SHOW_BOTTOM_BAR));
-            }
-        });
+        final NestedScrollView scrollView = (NestedScrollView) view.findViewById(R.id.scrollView);
 
 
         scrollView.scrollTo(0, 0);
 
-    }
+        pagerUser.setNestedScrollingEnabled(false);
+        pagerUser.setAdapter(userAdapter);
 
-    public void initUI(View view) {
-        btnProfile = (TextView) view.findViewById(R.id.btnProfile);
-        tvBirthday = (TextView) view.findViewById(R.id.tv_birthday);
-        tvHeight = (TextView) view.findViewById(R.id.tv_profile_height);
-        tvWeight = (TextView) view.findViewById(R.id.tv_profile_weight);
-        tvIBM = (TextView) view.findViewById(R.id.tv_profile_ibm);
-        tvName = (TextView) view.findViewById(R.id.tv_profile_name);
-        imgAvatar = (ImageView) view.findViewById(R.id.img_avatar);
-        imgGender = (ImageView) view.findViewById(R.id.img_gender);
-        progcess = (CoordinatorLayout) view.findViewById(R.id.progress_bar);
-
-    }
-
-    private void initControl() {
-        btnProfile.setOnClickListener(new View.OnClickListener() {
+        userAdapter.setItemUpDateClickListener(new PagerUserAdapter.ItemUpDateClickListener() {
             @Override
-            public void onClick(View view) {
-//                startActivity(new Intent(mContext, ProfileActivity.class));
+            public void onClick(int position, RESP_User user) {
+                startActivity(new Intent(mContext, ProfileActivity.class));
                 if (!isLogin())
                     showLoginDialog();
                 else
                     startActivityForResult(new Intent(mContext, ProfileActivity.class), Constant.UPDATE_PROFILE);
-//                getActivity().finish();
             }
         });
+
+        pagerUser.addScrollStateChangeListener(new DiscreteScrollView.ScrollStateChangeListener<RecyclerView.ViewHolder>() {
+            @Override
+            public void onScrollStart(@NonNull RecyclerView.ViewHolder currentItemHolder, int adapterPosition) {
+
+            }
+
+            @Override
+            public void onScrollEnd(@NonNull RecyclerView.ViewHolder currentItemHolder, int adapterPosition) {
+                if (adapterPosition < mlistUsers.size()) {
+                    rvDisease.setVisibility(View.VISIBLE);
+                } else {
+                    rvDisease.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onScroll(float scrollPosition, @NonNull RecyclerView.ViewHolder currentHolder, @NonNull RecyclerView.ViewHolder newCurrent) {
+
+            }
+        });
+    }
+
+    private void initControl() {
+//        btnProfile.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                startActivity(new Intent(mContext, ProfileActivity.class));
+//                if (!isLogin())
+//                    showLoginDialog();
+//                else
+//                    startActivityForResult(new Intent(mContext, ProfileActivity.class), Constant.UPDATE_PROFILE);
+////                getActivity().finish();
+//            }
+//        });
     }
 
     @Override
@@ -171,29 +202,31 @@ public class HomeFragment extends BasicFragment implements ScreenShotable, IHome
             setNull();
             return;
         }
-        tvName.setText(user.getFullname());
-        tvBirthday.setText(user.getBirthDayasString());
-        tvHeight.setText(String.valueOf(user.getHeight()));
-        tvWeight.setText(String.valueOf(user.getWeight()));
-        try {
-            DecimalFormat format = new DecimalFormat("##.##");
-            tvIBM.setText(format.format(user.getWeight() * 100 * 100 / (user.getHeight() * user.getHeight())));
-        } catch (Exception e) {
-            tvIBM.setText("0.0");
-        }
-
-        setImage(imgAvatar, user.getAvatar());
-        if (user.getGender() == 2) {
-            imgGender.setVisibility(View.VISIBLE);
-            imgGender.setImageResource(R.drawable.ic_action_name);
-        } else if (user.getGender() == 1) {
-            imgGender.setVisibility(View.VISIBLE);
-            imgGender.setImageResource(R.drawable.ic_man);
-        } else {
-            imgGender.setVisibility(View.GONE);
-        }
-        SharedPreferencesUtils.getInstance().saveUser(user);
-        progcess.setVisibility(View.GONE);
+        mlistUsers.add(user);
+        userAdapter.notifyDataSetChanged();
+//        tvName.setText(user.getFullname());
+//        tvBirthday.setText(user.getBirthDayasString());
+//        tvHeight.setText(String.valueOf(user.getHeight()));
+//        tvWeight.setText(String.valueOf(user.getWeight()));
+//        try {
+//            DecimalFormat format = new DecimalFormat("##.##");
+//            tvIBM.setText(format.format(user.getWeight() * 100 * 100 / (user.getHeight() * user.getHeight())));
+//        } catch (Exception e) {
+//            tvIBM.setText("0.0");
+//        }
+//
+//        setImage(imgAvatar, user.getAvatar());
+//        if (user.getGender() == 2) {
+//            imgGender.setVisibility(View.VISIBLE);
+//            imgGender.setImageResource(R.drawable.ic_action_name);
+//        } else if (user.getGender() == 1) {
+//            imgGender.setVisibility(View.VISIBLE);
+//            imgGender.setImageResource(R.drawable.ic_man);
+//        } else {
+//            imgGender.setVisibility(View.GONE);
+//        }
+//        SharedPreferencesUtils.getInstance().saveUser(user);
+//        progcess.setVisibility(View.GONE);
 
     }
 
@@ -232,33 +265,33 @@ public class HomeFragment extends BasicFragment implements ScreenShotable, IHome
                 adapter.updateItem(data.getStringExtra(Constant.MEDICAL_NAME), data.getLongExtra(Constant.MEDICAL_BEGIN_TIME, -1), data.getIntExtra(Constant.MEDICAL_INDEX, -1));
             }
         } else if (requestCode == Constant.UPDATE_PROFILE) {
-            if (resultCode == Activity.RESULT_OK) {
-                RESP_User user = SharedPreferencesUtils.getInstance().getUser();
-                tvName.setText(user.getFullname());
-                tvBirthday.setText(user.getBirthDayasString());
-                tvHeight.setText(String.valueOf(user.getHeight()));
-                tvWeight.setText(String.valueOf(user.getWeight()));
-                setImage(imgAvatar, user.getAvatar());
-                if (user.getGender() == 2) {
-                    imgGender.setImageResource(R.drawable.ic_action_name);
-                    imgGender.setVisibility(View.VISIBLE);
-                } else if (user.getGender() == 1) {
-                    imgGender.setImageResource(R.drawable.ic_man);
-                    imgGender.setVisibility(View.VISIBLE);
-                } else {
-                    imgGender.setVisibility(View.GONE);
-                }
-            }
+//            if (resultCode == Activity.RESULT_OK) {
+//                RESP_User user = SharedPreferencesUtils.getInstance().getUser();
+//                tvName.setText(user.getFullname());
+//                tvBirthday.setText(user.getBirthDayasString());
+//                tvHeight.setText(String.valueOf(user.getHeight()));
+//                tvWeight.setText(String.valueOf(user.getWeight()));
+//                setImage(imgAvatar, user.getAvatar());
+//                if (user.getGender() == 2) {
+//                    imgGender.setImageResource(R.drawable.ic_action_name);
+//                    imgGender.setVisibility(View.VISIBLE);
+//                } else if (user.getGender() == 1) {
+//                    imgGender.setImageResource(R.drawable.ic_man);
+//                    imgGender.setVisibility(View.VISIBLE);
+//                } else {
+//                    imgGender.setVisibility(View.GONE);
+//                }
+//            }
         }
     }
 
     private void setNull() {
-        tvName.setText(null);
-        tvBirthday.setText(null);
-        tvHeight.setText(null);
-        tvWeight.setText(null);
-        imgGender.setVisibility(View.GONE);
-        progcess.setVisibility(View.GONE);
+//        tvName.setText(null);
+//        tvBirthday.setText(null);
+//        tvHeight.setText(null);
+//        tvWeight.setText(null);
+//        imgGender.setVisibility(View.GONE);
+//        progcess.setVisibility(View.GONE);
 
     }
 
